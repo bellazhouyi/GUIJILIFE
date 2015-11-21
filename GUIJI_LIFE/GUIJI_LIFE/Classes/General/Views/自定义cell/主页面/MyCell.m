fb814eed14d6efadcfca412c7829634b52f2908a
xuan <baoxuan_hu@foxmail.com>
2015-11-21 10:02:21 +0800

    2015.11.20  HBX  提交 输入字数限制

--- GUIJI_LIFE/GUIJI_LIFE/Classes/General/Views/自定义cell/主页面/MyCell.m
+++ GUIJI_LIFE/GUIJI_LIFE/Classes/General/Views/自定义cell/主页面/MyCell.m
@@ -24,13 +24,54 @@
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
     if (self = [super initWithCoder:aDecoder]) {
-    
+        
+        // 注册notification
+        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(textFiledEditChanged:)
+                                                    name:@"UITextFieldTextDidChangeNotification"
+                                                  object:self.addTextField];
         
     }
     return self;
 }
 
 
+#define kMaxLength 50
+// 实现监听方法
+-(void)textFiledEditChanged:(NSNotification *)obj{
+    UITextField *textField = (UITextField *)obj.object;
+    
+    NSString *toBeString = textField.text;
+    NSString *lang = [[UITextInputMode currentInputMode] primaryLanguage]; // 键盘输入模式
+    if ([lang isEqualToString:@"zh-Hans"]) { // 简体中文输入，包括简体拼音，健体五笔，简体手写
+        UITextRange *selectedRange = [textField markedTextRange];
+        //获取高亮部分
+        UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
+        // 没有高亮选择的字，则对已输入的文字进行字数统计和限制
+        if (!position) {
+            if (toBeString.length > kMaxLength) {
+                textField.text = [toBeString substringToIndex:kMaxLength];
+            }
+        }
+        // 有高亮选择的字符串，则暂不对文字进行统计和限制
+        else{
+            
+        }
+    }
+    // 中文输入法以外的直接对其统计限制即可，不考虑其他语种情况
+    else{
+        if (toBeString.length > kMaxLength) {
+            textField.text = [toBeString substringToIndex:kMaxLength];
+        }
+    }
+}
+
+
+// 在dealloc里注销掉监听方法
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self
+                                                   name:@"UITextFieldTextDidChangeNotification"
+                                                 object:self.addTextField];
+}
 // 闹钟开关按钮
 - (IBAction)ClockAction:(UISwitch *)sender {
     
@@ -282,7 +323,6 @@
 
     
 }
-
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
